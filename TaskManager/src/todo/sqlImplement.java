e842dfc1bb55de05e5f448325251ecc7cde6cd1f
shawn <shawn@10.228.8.40>
2024-11-24 12:21:59 -0500

    Initial coding for database integration into Task Manager

--- TaskManager/src/todo/sqlImplement.java
+++ TaskManager/src/todo/sqlImplement.java
@@ -1,9 +1,16 @@
 package todo;
 
+import java.sql.Connection;
+import java.sql.*;
+import java.util.Scanner;
 public class sqlImplement {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//code to implement sql database into taskManager using WampServer
+	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		
+		Class.forName("com.mysql.jdc.Driver"); //loads the java database connector driver class
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/filler","root",""); //filler is a place holder, change when table is created
+		
 
 	}
 
